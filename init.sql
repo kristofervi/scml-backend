f2b3956ea6e4554549096ae58fca10d260c5b3b1
KristoferVildin <kristofer.vildin@gmail.com>
2025-10-26 19:34:34 +0100

    lagt till spelare, uppdaterat init.sql

--- init.sql
+++ init.sql
@@ -10,3 +10,12 @@ INSERT INTO players (name, points) VALUES
 ('Jack Eichel', 16),
 ('Nick Schmaltz', 15),
 ('William Nylander', 14);
+
+INSERT INTO players (name, points) VALUES
+('Evgeni Malkin', 14),
+('Mark Stone', 13),
+('Dylan Larkin', 13),
+('Nathan MacKinnon', 13),
+('Nick Suzuki', 13),
+('David Pastrnak', 13),
+('Macklin Celebrini', 12);
